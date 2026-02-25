4790643493ec5c26265aea3a9a42de69e0c3e268
João Neto <1539767+joaomlneto@users.noreply.github.com>
2026-02-25 17:41:02 +0100

    D3

--- D3/get-heroes/src/helpers/getOwnedHeroesIds.ts
+++ D3/get-heroes/src/helpers/getOwnedHeroesIds.ts
@@ -1,11 +1,26 @@
-import { ENV } from "../env";
-import { suiClient } from "../suiClient";
+import {ENV} from "@/env";
+import {suiClient} from "../suiClient";
 
 /**
  * Gets all of the Hero NFTs owned by the given address.
  * Returns an array of their Object Ids.
  */
 export const getOwnedHeroesIds = async (owner: string) => {
-  // TODO: Implement this function
-  return [];
+  let response = await suiClient.listOwnedObjects({
+    owner,
+    type: `${ENV.PACKAGE_ID}::hero::Hero`,
+  })
+
+  let heroIds = response.objects.map(({objectId}) => objectId);
+
+  while (response.hasNextPage) {
+    response = await suiClient.listOwnedObjects({
+      owner,
+      type: `${ENV.PACKAGE_ID}::hero::Hero`,
+      cursor: response.cursor,
+    });
+    heroIds = [...heroIds, ...response.objects.map(({objectId}) => objectId)];
+  }
+
+  return heroIds;
 };
